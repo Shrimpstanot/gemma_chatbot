d2003d2bf57c655108ba310f3645fd00209e2428
Abdurrahman Javat <a.javat70@gmail.com>
2025-04-13 20:08:40 +0300

    added html/flask/data

---
+++ 1_bus_route_lookup_app/backend/data.py
@@ -0,0 +1,15 @@
+# backend/data.py
+bus_routes = [
+    {
+        "route": "34A",
+        "stops": ["A", "B", "C", "D", "E"]
+    },
+    {
+        "route": "97",
+        "stops": ["E", "F", "G", "H"]
+    },
+    {
+        "route": "500T",
+        "stops": ["A", "X", "F", "Y", "Z"]
+    }
+]
---
+++ 1_bus_route_lookup_app/backend/main.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify, request
+from data import bus_routes
+
+app = Flask(__name__)
+
+@app.route("/get-route", methods = ["POST"])
+def get_route():
+    data = request.json
+    start = data.get("start")
+    destination = data.get("destination")
+    
+    possible_routes = []
+    for route in bus_routes:
+        stops = route["stops"]
+        if start in stops and destination in stops and stops.index(start) < stops.index(destination):
+            possible_routes.append(route["route"])
+    return jsonify({"routes": possible_routes})
+
+if __name__ == "__main__":
+    get_route()
