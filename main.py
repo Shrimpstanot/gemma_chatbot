a1fc4488bf300934bd69c16fb90513335943f42a
Abdurrahman Javat <a.javat70@gmail.com>
2025-06-23 11:54:29 +0300

    working gemma3 using google ai sdk

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
+#main.py
 from fastapi import FastAPI
+from pydantic import BaseModel
+from google import genai
+import dotenv
+import os
+
+# Set the environment variable for the Gemma API key
+dotenv.load_dotenv(dotenv_path=".env")
+GEMMA_API_KEY = os.getenv("GEMMA_API_KEY")
+
+class ChatMessage(BaseModel):
+    message: str
 
 app = FastAPI()
 
@@ -7,3 +19,10 @@ app = FastAPI()
 async def root():
     return {"message": "Hello World"}
 
+@app.post("/chat")
+async def create_chat_message(user_message: ChatMessage):
+    client = genai.Client(api_key=GEMMA_API_KEY)
+    response = client.models.generate_content(
+        model="gemma-3-27b-it", contents=[user_message.message]
+    )
+    return {"response": response.text}
