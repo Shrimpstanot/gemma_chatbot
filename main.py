99acf8207c300381bdb6335deefd20173896cb70
Abdurrahman Javat <a.javat70@gmail.com>
2025-06-23 12:58:43 +0300

    added image + caption endpoint

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, File, Form, UploadFile
 from pydantic import BaseModel
 from google import genai
+from google.genai import types
 import dotenv
 import os
 
@@ -26,5 +27,23 @@ async def root():
 async def create_chat_message(user_message: ChatMessage):
     response = client.models.generate_content(
         model="gemma-3-27b-it", contents=[user_message.message]
+    )
+    return {"response": response.text}
+
+@app.post("/chat-with-image")
+async def chat_with_image(
+    caption: str = Form(...),
+    image: UploadFile = File(...)
+):
+    user_file = await image.read()
+    response = client.models.generate_content(
+        model="gemma-3-27b-it",
+        contents=[
+            types.Part.from_bytes(
+                data=user_file,
+                mime_type="image/jpeg",
+            ),
+            caption
+        ]
     )
     return {"response": response.text}
